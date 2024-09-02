b79ef2eb3a7248b26dc1992b878bcaf34a8090ea
Amar <onlyaboutcoc@gmail.com>
2024-09-02 16:20:45 +0530

    fixed the landing page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 
 import Navbar from "./components/Navbar";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation';
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 
-export default function Home() {
+export default async function Home() {
 
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/dashboard');
+  } 
   return (
     <>
       <Navbar />
